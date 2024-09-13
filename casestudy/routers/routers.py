e56f7b2a56b4b483707c0bf3af378ab6f9e98eb4
vrittech-manoj <manoj@vrittechnologies.com>
2024-09-13 16:15:53 +0545

    api format

--- casestudy/routers/routers.py
+++ casestudy/routers/routers.py
@@ -7,6 +7,6 @@ router = DefaultRouter()
 auto_api_routers = router
 
 
-router.register('casestudytags', casestudytagsViewsets, basename="casestudytagsViewsets")
-router.register('casestudycategory', casestudycategoryViewsets, basename="casestudycategoryViewsets")
+router.register('casestudy-tags', casestudytagsViewsets, basename="casestudytagsViewsets")
+router.register('cases-tudy-category', casestudycategoryViewsets, basename="casestudycategoryViewsets")
 router.register('casestudy', casestudyViewsets, basename="casestudyViewsets")
