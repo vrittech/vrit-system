8b408fb4b66d99ab9497a43d96fbbb59b1ad5ee4
vrittech-manoj <manoj@vrittechnologies.com>
2024-09-13 15:57:31 +0545

    deployment add static

--- vrittech/settings.py
+++ vrittech/settings.py
@@ -135,7 +135,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+import os
+
+STATIC_URL = '/static/'
+# ADMIN_MEDIA_PREFIX = '/static/admin/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
