fc630a7b71c5edc603b74df482a14e0dca38c3bf
vrittech-manoj <manoj@vrittechnologies.com>
2024-09-13 11:26:53 +0545

    swagger

--- vrittech/settings.py
+++ vrittech/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'drf_yasg',
     'drfapigenerator',
     'accounts',
     'blog',
