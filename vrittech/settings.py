9bc015854d2e0bd718faf27d313b6f3c145cd992
vrittech-manoj <manoj@vrittechnologies.com>
2024-09-13 15:59:36 +0545

    deployment add static

--- vrittech/settings.py
+++ vrittech/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z_v50#t3ksa39g*-$g_bj00(lw^st%@u68_xmcp!9wk*iuf*q$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.237.94.113']
 
 
 # Application definition
