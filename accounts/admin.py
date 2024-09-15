65959d3e48e2182528ca2a959d2c2fb6b8aadb38
vrittech-manoj <manoj@vrittechnologies.com>
2024-09-15 15:48:34 +0545

    department

--- accounts/admin.py
+++ accounts/admin.py
@@ -10,7 +10,7 @@ class UserAdmin(BaseUserAdmin):
     exclude = ('user_permissions',)
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
-        ('Personal info', {'fields': ('first_name', 'last_name', 'email','avatar')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email','avatar','department')}),
         ('Permissions', {'fields': ('is_active','is_staff', 'is_superuser','groups')}),
         # ('Social sites',{'fields':('facebook','twitter','tiktok','instagram','youtube')})
         # Add your custom fields here
