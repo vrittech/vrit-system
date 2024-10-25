0534e0d273298b6de839c4802afa7f3c78fcebc4
Prashant Karna <prashant@vrittechnologies.com>
2024-10-25 12:26:22 +0545

    issue fixed

--- accountsmanagement/action.py
+++ accountsmanagement/action.py
@@ -1,29 +1,29 @@
-from booking.models import DestinationBook
-from django.utils.http import urlsafe_base64_encode
-from django.utils.encoding import force_bytes
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from django.core.mail import send_mail
-from accounts.models import CustomUser
-from accountsmanagement.views import send_booking_confirmation_email
+# from booking.models import DestinationBook
+# from django.utils.http import urlsafe_base64_encode
+# from django.utils.encoding import force_bytes
+# from django.db.models.signals import post_save
+# from django.dispatch import receiver
+# from django.core.mail import send_mail
+# from accounts.models import CustomUser
+# from accountsmanagement.views import send_booking_confirmation_email
 
 
-@receiver(post_save, sender=DestinationBook)
-def booking_created_handler(sender, instance, created, **kwargs):
-    if created:
-        # Construct the verification URL
-        site_url = 'https://example.com'  
-        verify_url = f"{site_url}/user-verification-success?pk={urlsafe_base64_encode(force_bytes(instance.pk))}"
+# @receiver(post_save, sender=DestinationBook)
+# def booking_created_handler(sender, instance, created, **kwargs):
+#     if created:
+#         # Construct the verification URL
+#         site_url = 'https://example.com'  
+#         verify_url = f"{site_url}/user-verification-success?pk={urlsafe_base64_encode(force_bytes(instance.pk))}"
 
-        # Fetch the admin email from the User model
-        admin_user = CustomUser.objects.filter(is_superuser=True).first()
-        if admin_user:
-            admin_name = admin_user.first_name
-            admin_email = admin_user.email
-        else:
-            raise ValueError("Admin email not found.")
+#         # Fetch the admin email from the User model
+#         admin_user = CustomUser.objects.filter(is_superuser=True).first()
+#         if admin_user:
+#             admin_name = admin_user.first_name
+#             admin_email = admin_user.email
+#         else:
+#             raise ValueError("Admin email not found.")
 
-        # Send the confirmation email
-        subject = 'Booking Verification Email'
-        send_booking_confirmation_email(instance.email, verify_url, subject, instance, admin_email, admin_name)
+#         # Send the confirmation email
+#         subject = 'Booking Verification Email'
+#         send_booking_confirmation_email(instance.email, verify_url, subject, instance, admin_email, admin_name)
 
--- accountsmanagement/urls.py
+++ accountsmanagement/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from .views import EmailCheckView, CustomPasswordResetView , VerifyUserPasswordToken,ContactmeView,EmailChangeGetOtpView,EmailResetView
-from .views import  SendEmailForBookingVerification, PasswordResetView
+from .views import PasswordResetView
 
 
 
 urlpatterns = [
     path('get-otp/', EmailCheckView.as_view()),
     path('password-reset-get-otp/', PasswordResetView.as_view()),
-    path('send-booking-confirmation/', SendEmailForBookingVerification.as_view()),
+    # path('send-booking-confirmation/', SendEmailForBookingVerification.as_view()),
     path('get-otp-email-change/', EmailChangeGetOtpView.as_view()),
     path('email-reset/', EmailResetView.as_view(), name="EmailResetView"),
     path('password-reset/', CustomPasswordResetView.as_view(), name="reset-password"),
