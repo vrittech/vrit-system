1822430c3a93b9687c43ec2b51f8f6654e8b9733
Prashant Karna <prashant@vrittechnologies.com>
2024-10-28 11:30:00 +0545

    celery implemented

--- vrittech/celery.py
+++ vrittech/celery.py
@@ -2,9 +2,15 @@ import os
 from celery import Celery
 from django.conf import settings
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vrittech.settings')
 
-app = Celery('your_project')
+app = Celery('vrittech')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
+
+# Run the Celery worker:
+# celery -A your_project worker --loglevel=info
+
+# Run Celery Beat (to enable periodic tasks):
+# celery -A your_project beat --loglevel=info
