9c51f5dbd5aac3c76809ecd6770131d175a8c4b7
Prashant Karna <prashant@vrittechnologies.com>
2024-10-28 11:27:17 +0545

    blog api

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from accounts.models import CustomUser
 import ast
-
+from django.utils import timezone
 class BlogTags(models.Model):
     name = models.CharField(max_length = 155)
     
