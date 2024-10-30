ba15611cddd6b805d04b2833cffe5132682bf41f
Prashant Karna <prashant@vrittechnologies.com>
2024-10-30 11:26:52 +0545

    "Updated serializers for Testimonial, Gallery, and GlobalPresence models to include validation for position uniqueness and image handling in update method."

--- gallery/serializers/gallery_serializers.py
+++ gallery/serializers/gallery_serializers.py
@@ -15,3 +15,23 @@ class GalleryWriteSerializers(serializers.ModelSerializer):
     class Meta:
         model = Gallery
         fields = '__all__'
+        
+    def update(self, instance, validated_data):
+        image = validated_data.pop('image', None)
+
+        # Update instance fields if data is provided
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        # Handle the image field specifically
+        if image is not None:
+            if image == "null":
+                # If image is set to 'null', delete the current image
+                instance.image.delete(save=False)
+                instance.image = None
+            else:
+                # If image data is sent, update it
+                instance.image = image
+
+        instance.save()
+        return instance
--- testimonial/serializers/testimonial_serializers.py
+++ testimonial/serializers/testimonial_serializers.py
@@ -20,5 +20,25 @@ class TestimonialWriteSerializers(serializers.ModelSerializer):
         # Check if the position already exists in another collection
         position = data.get('position')
         if Testimonial.objects.filter(position=position).exists():
-            raise serializers.ValidationError({"A collection with this position already exists."})
+            raise serializers.ValidationError({"A Testimonial with this position already exists."})
         return data
+    
+    def update(self, instance, validated_data):
+        image = validated_data.pop('image', None)
+
+        # Update instance fields if data is provided
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        # Handle the image field specifically
+        if image is not None:
+            if image == "null":
+                # If image is set to 'null', delete the current image
+                instance.image.delete(save=False)
+                instance.image = None
+            else:
+                # If image data is sent, update it
+                instance.image = image
+
+        instance.save()
+        return instance
