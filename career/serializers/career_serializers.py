2c1295e297d8c3e1268afedd97cfdaba552a553f
Prashant Karna <prashant@vrittechnologies.com>
2024-10-30 11:10:28 +0545

    career image is done

--- career/serializers/career_serializers.py
+++ career/serializers/career_serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from ..models import Career,ExperienceLevel
+from ..models import Career, ExperienceLevel
 
 class ExperienceLevelSerializers_Career(serializers.ModelSerializer):
     class Meta:
@@ -8,12 +8,14 @@ class ExperienceLevelSerializers_Career(serializers.ModelSerializer):
 
 class CareerListSerializers(serializers.ModelSerializer):
     experience_level = ExperienceLevelSerializers_Career()
+
     class Meta:
         model = Career
         fields = '__all__'
 
 class CareerRetrieveSerializers(serializers.ModelSerializer):
     experience_level = ExperienceLevelSerializers_Career()
+
     class Meta:
         model = Career
         fields = '__all__'
@@ -24,8 +26,28 @@ class CareerWriteSerializers(serializers.ModelSerializer):
         fields = '__all__'
     
     def validate(self, data):
-        # Check if the position already exists in another collection
+        # Check if the position already exists in another Career
         position = data.get('position')
-        if Career.objects.filter(position=position).exists():
-            raise serializers.ValidationError({"A collection with this position already exists."})
+        if position and Career.objects.filter(position=position).exists():
+            raise serializers.ValidationError({"position": "A career with this position already exists."})
         return data
+
+    def update(self, instance, validated_data):
+        # Handle the image field separately
+        image = validated_data.pop('image', None)
+
+        if image is not None:
+            if image == "null":
+                # If image is set to 'null', delete the current image
+                instance.image.delete(save=False)
+                instance.image = None
+            else:
+                # If image data is sent, update it
+                instance.image = image
+
+        # Update other fields
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
