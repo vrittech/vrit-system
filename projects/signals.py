db56e527416590932c8d709fb8fbb16432772f5a
Prashant Karna <prashant@vrittechnologies.com>
2024-11-05 12:41:54 +0545

    signals updated for group and services

--- projects/signals.py
+++ projects/signals.py
@@ -2,10 +2,23 @@
 
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Project
+from .models import Project,ProjectGroup,ProjectService
 
 @receiver(post_save, sender=Project)
 def set_position_same_as_id(sender, instance, created, **kwargs):
     if created and instance.position != instance.id:
         instance.position = instance.id
         instance.save()
+
+@receiver(post_save, sender=ProjectGroup)
+def set_position_same_as_id(sender, instance, created, **kwargs):
+    if created and instance.position != instance.id:
+        instance.position = instance.id
+        instance.save()
+        
+@receiver(post_save, sender=ProjectService)
+def set_position_same_as_id(sender, instance, created, **kwargs):
+    if created and instance.position != instance.id:
+        instance.position = instance.id
+        instance.save()
+
