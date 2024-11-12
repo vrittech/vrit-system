91adaba3f65f0baca9df020707aa7d435a723e64
Prashant Karna <prashant@vrittechnologies.com>
2024-11-12 15:10:40 +0545

    bulk data can be added in country now

--- globalpresence/serializers/country_serializers.py
+++ globalpresence/serializers/country_serializers.py
@@ -12,6 +12,41 @@ class CountryRetrieveSerializers(serializers.ModelSerializer):
         fields = '__all__'
 
 class CountryWriteSerializers(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
+
     class Meta:
         model = Country
-        fields = '__all__'
+        fields = ['id', 'name']
+
+    def create(self, validated_data):
+        countries_data = validated_data.get('countries', [])
+        created_countries = []
+
+        for country_data in countries_data:
+            # Create a new country if no ID is provided
+            if 'id' not in country_data:
+                country = Country.objects.create(name=country_data['name'])
+                created_countries.append(country)
+
+        return created_countries
+
+    def update(self, instance, validated_data):
+        countries_data = validated_data.get('countries', [])
+        updated_countries = []
+
+        for country_data in countries_data:
+            country_id = country_data.get('id')
+
+            # Update an existing country if ID is provided
+            if country_id:
+                country = Country.objects.filter(id=country_id).first()
+                if country:
+                    country.name = country_data['name']
+                    country.save()
+                    updated_countries.append(country)
+                else:
+                    # Optionally, handle the case where the ID does not exist
+                    # Raise an exception or handle as needed
+                    pass
+
+        return updated_countries
