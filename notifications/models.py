2552eab9aacd66be58c0d87518b503f9700e4898
Prashant Karna <prashant@vrittechnologies.com>
2024-11-13 17:04:18 +0545

    issue fixed

--- notifications/models.py
+++ notifications/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.utils import timezone
+from accounts.models import CustomUser
 
-User = get_user_model()
+# User = get_user_model()
 
 class Notification(models.Model):
     title = models.CharField(max_length=255)
     message = models.TextField()
     created_at = models.DateTimeField(default=timezone.now)
     is_read = models.BooleanField(default=False)
-    users = models.ManyToManyField(User, related_name="notifications")
+    users = models.ManyToManyField(CustomUser, related_name="notifications")
     module_name = models.CharField(max_length=100)
 
     def __str__(self):
