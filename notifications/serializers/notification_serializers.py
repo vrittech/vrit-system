93b692839d6bb2d2ecc38df1244b73945f253cb5
Prashant Karna <prashant@vrittechnologies.com>
2024-11-13 17:53:33 +0545

    notification issue fixed of auth migrations and api created

--- notifications/serializers/notification_serializers.py
+++ notifications/serializers/notification_serializers.py
@@ -1,17 +1,17 @@
-# from rest_framework import serializers
-# from ..models import Notification
+from rest_framework import serializers
+from ..models import Notification
 
-# class NotificationListSerializers(serializers.ModelSerializer):
-#     class Meta:
-#         model = Notification
-#         fields = '__all__'
+class NotificationListSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Notification
+        fields = '__all__'
 
-# class NotificationRetrieveSerializers(serializers.ModelSerializer):
-#     class Meta:
-#         model = Notification
-#         fields = '__all__'
+class NotificationRetrieveSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Notification
+        fields = '__all__'
 
-# class NotificationWriteSerializers(serializers.ModelSerializer):
-#     class Meta:
-#         model = Notification
-#         fields = '__all__'
+class NotificationWriteSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Notification
+        fields = '__all__'
