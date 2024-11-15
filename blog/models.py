e33a5761f8831129522017ba33e5781df6edfe3e
Prashant Karna <prashant@vrittechnologies.com>
2024-11-15 15:51:07 +0545

    blog scheduling

--- blog/models.py
+++ blog/models.py
@@ -96,14 +96,14 @@ class Blog(models.Model):
     def __str__(self):
         return self.title
     
-    def publish_if_scheduled(self):
-        if self.status == 'scheduled' and self.publish_date <= timezone.now().date():
-            self.status = 'published'
-            self.save()
+    # def publish_if_scheduled(self):
+    #     if self.status == 'scheduled' and self.publish_date <= timezone.now().date():
+    #         self.status = 'published'
+    #         self.save()
 
-    @classmethod
-    def publish_scheduled_blogs(cls):
-        scheduled_blogs = cls.objects.filter(status='scheduled', publish_date__lte=timezone.now().date())
-        for blog in scheduled_blogs:
-            blog.publish_if_scheduled()
+    # @classmethod
+    # def publish_scheduled_blogs(cls):
+    #     scheduled_blogs = cls.objects.filter(status='scheduled', publish_date__lte=timezone.now().date())
+    #     for blog in scheduled_blogs:
+    #         blog.publish_if_scheduled()
             
