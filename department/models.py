0e521dc4b3b6b3227eb00d7be66a2b6ee5725684
Prashant Karna <prashant@vrittechnologies.com>
2024-11-17 14:58:48 +0545

    department api updated

--- department/models.py
+++ department/models.py
@@ -3,6 +3,10 @@ from django.db import models
 # Create your models here.
 class Department(models.Model):
     name = models.CharField(max_length=100)
+    media = models.ImageField(upload_to="gallery",null=True,blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
 
     def __str__(self):
         return self.name
